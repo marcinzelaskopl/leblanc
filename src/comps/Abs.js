b39663435dd111bbf233c712159275c3d560ab7b
marcinzelaskopl <marcinzelaskov@gmail.com>
2018-07-26 12:10:57 +0200

    added animations

--- src/comps/Abs.js
+++ src/comps/Abs.js
@@ -10,6 +10,7 @@ import W from '../images/w.png'
 import VideoW  from '../videos/w.webm'
 import R from '../images/mimic.png'
 import VideoR from '../videos/r.webm'
+import Slide from 'react-reveal/Slide';
 
 
 class Abs extends Component {
@@ -22,54 +23,74 @@ class Abs extends Component {
             <div className={'grid-container abs'}>
                 <div className={'row'}>
                     <div className={'col-12 abs-info'}>
+                        <Slide left>
                         <img src={ImgPassive} alt={'passive'}/>
                         <h1>Mirror Image</h1>
                         <p>When LeBlanc drops below 40% health, she becomes invisible for 1 second and creates a Mirror Image that deals no damage and lasts for up to 8 seconds. Mirror Image has a 1 minute cooldown.</p>
+                        </Slide>
                     </div>
                     <div className={'col-12 vid'}>
+                        <Slide right>
                         <video id={'abs'} controls={'controls'}>
                             <source src={VideoPassive}/>
                         </video>
+                        </Slide>
                     </div>
                     <div className={'col-12 abs-info'}>
+                        <Slide left>
                         <img src={qPassive} alt={'passive'}/>
                         <h1>Sigil of Malice</h1>
                         <p>LeBlanc projects a sigil, dealing damage and marking the target for 3.5 seconds. Damaging the marked target with an ability detonates the sigil, dealing additional damage. LeBlanc projects a sigil, dealing 55/80/105/130/155 (+40% Ability Power) magic damage and marking the target for 3.5 seconds.Damaging the marked target with an ability detonates the sigil, dealing 55/80/105/130/155 (+40% Ability Power) magic damage.</p>
+                        </Slide>
                     </div>
                     <div className={'col-12 vid'}>
+                        <Slide right>
                         <video controls={'controls'}>
                             <source src={VideoQ}/>
                         </video>
+                        </Slide>
                     </div>
                     <div className={'col-12 abs-info'}>
+                        <Slide left>
                         <img src={W} alt={'passive'}/>
                         <h1>Distortion</h1>
                         <p>LeBlanc dashes to a location, dealing damage to enemies near her destination. For the next 4 seconds, activate Distortion again to return LeBlanc to her starting location. LeBlanc dashes to a location, dealing 85/125/165/205/245 (+60% Ability Power) magic damage to enemies near her destination.For the next 4 seconds, activate Distortion again to return LeBlanc to her starting location.</p>
+                        </Slide>
                     </div>
                     <div className={'col-12 vid'}>
+                        <Slide right>
                         <video controls={'controls'}>
                             <source src={VideoW}/>
                         </video>
+                        </Slide>
                     </div>
                     <div className={'col-12 abs-info'}>
+                        <Slide left>
                         <img src={E} alt={'passive'}/>
                         <h1>Ethereal Chains</h1>
                         <p>LeBlanc launches a chain that shackles the first enemy hit. If the target remains shackled for 1.5 seconds, LeBlanc roots them and deals additional damage. eBlanc launches a chain that shackles the first enemy hit, dealing 40/60/80/100/120 (+30% Ability Power) magic damage and granting True Sight of the target.If the target remains shackled for 1.5 seconds, LeBlanc roots them for 1.5 seconds and deals an additional 60/90/120/150/180 (+70% Ability Power) magic damage.</p>
+                        </Slide>
                     </div>
                     <div className={'col-12 vid'}>
+                        <Slide right>
                         <video controls={'controls'}>
                             <source src={VideoE}/>
                         </video>
+                        </Slide>
                     </div>
                     <div className={'col-12 abs-info'}>
+                        <Slide left>
                         <img className={'mimic'} src={R} alt={'passive'}/>
                         <h1>Mimic</h1>
                         <p>LeBlanc casts a mimicked version of one of her basic spells. LeBlanc mimics her most recent spell, casting it again.Mimicked Sigil of Malice and Ethereal Chains deal magic damage, plus magic damage from their mark and root, respectively. Mimicked Distortion deals magic damage.</p>
+                        </Slide>
                     </div>
                     <div className={'col-12 vid'}>
+                        <Slide right>
                         <video controls={'controls'}>
                             <source src={VideoR}/>
                         </video>
+                        </Slide>
                     </div>
                     <div className={'grid-container'}>
                         <div className={'row'}>
